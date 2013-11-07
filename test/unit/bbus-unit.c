00f88b44096a1aaf72b8f0a7ebc64f995a56dfc2
Bartosz Golaszewski <bartekgola@gmail.com>
2013-11-07 20:38:07 +0100

    bbus-unit: measure time spent on testing.

--- test/unit/bbus-unit.c
+++ test/unit/bbus-unit.c
@@ -21,6 +21,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <sys/time.h>
 
 struct testlist
 {
@@ -28,10 +30,8 @@ struct testlist
 	struct bbusunit_listelem* tail;
 };
 
-static unsigned tests_registered = 0;
-static unsigned tests_run = 0;
-static unsigned tests_failed = 0;
 static struct testlist tests;
+static unsigned tests_registered = 0;
 
 static BBUS_ATSTART_FIRST void testlist_init(void)
 {
@@ -72,10 +72,44 @@ void bbusunit_printerr(const char* fmt, ...)
 	PRINT_FROM_VA(stderr, "[ERROR]\t", fmt);
 }
 
+static void timeval_diff(struct timeval* res,
+				const struct timeval* x,
+				const struct timeval* y)
+{
+	struct timeval tmp;
+
+	tmp.tv_sec = y->tv_sec;
+	tmp.tv_usec = y->tv_usec;
+
+	if (x->tv_usec < tmp.tv_usec) {
+		int nsec = (tmp.tv_usec - x->tv_usec) / 1000000 + 1;
+		tmp.tv_usec -= 1000000 * nsec;
+		tmp.tv_sec += nsec;
+	}
+
+	if (x->tv_usec - tmp.tv_usec > 1000000) {
+		int nsec = (x->tv_usec - tmp.tv_usec) / 1000000;
+		tmp.tv_usec += 1000000 * nsec;
+		tmp.tv_sec -= nsec;
+	}
+
+	res->tv_sec = x->tv_sec - tmp.tv_sec;
+	res->tv_usec = x->tv_usec - tmp.tv_usec;
+}
+
 int main(int argc BBUS_UNUSED, char** argv BBUS_UNUSED)
 {
 	struct bbusunit_listelem* el;
 	int r;
+	unsigned tests_run = 0;
+	unsigned tests_failed = 0;
+	struct timeval begin;
+	struct timeval end;
+	struct timeval time_spent;
+
+	memset(&begin, 0, sizeof(struct timeval));
+	memset(&end, 0, sizeof(struct timeval));
+	memset(&time_spent, 0, sizeof(struct timeval));
 
 	bbusunit_print("##############################");
 	bbusunit_print("####> Busybus unit-tests <####");
@@ -83,6 +117,7 @@ int main(int argc BBUS_UNUSED, char** argv BBUS_UNUSED)
 	bbusunit_print("%u tests registered.", tests_registered);
 	bbusunit_print("Running tests...");
 	el = tests.head;
+	gettimeofday(&begin, NULL);
 	while (el != NULL) {
 		bbusunit_print("Case:\t[%s]", el->name);
 		r = el->testfunc();
@@ -93,11 +128,16 @@ int main(int argc BBUS_UNUSED, char** argv BBUS_UNUSED)
 		++tests_run;
 		el = el->next;
 	}
+	gettimeofday(&end, NULL);
+	timeval_diff(&time_spent, &end, &begin);
 	bbusunit_print("All done!");
 	bbusunit_print("SUMMARY:");
-	bbusunit_print("  Tests run:\t%d", tests_run);
+	bbusunit_print("  %u %s run in %d.%06d seconds.", tests_run,
+			tests_run == 1 ? "test" : "tests",
+			(int)time_spent.tv_sec, (int)time_spent.tv_usec);
 	if (tests_failed > 0) {
-		bbusunit_print("  Tests failed:\t%d", tests_failed);
+		bbusunit_print("  %u %s failed", tests_failed,
+				tests_failed == 1 ? "test" : "tests");
 		r = EXIT_FAILURE;
 	} else {
 		bbusunit_print("  All tests PASSED!");
