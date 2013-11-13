0bb9bff8036ea1a8c26d130d3d99d2e1e5b52d2b
Bartosz Golaszewski <bartekgola@gmail.com>
2013-11-13 18:47:42 +0100

    Add e-mail address to the copyright statements.

--- test/regression/regression.py
+++ test/regression/regression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2013 Bartosz Golaszewski
+# Copyright (C) 2013 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
